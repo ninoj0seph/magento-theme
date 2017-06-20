ead61ec4321d02b4edfb8a2b656720ad1774cdc1
ninoj0seph <ninojoseph.tugade@gmail.com>
2017-06-20 14:13:32 -0700

    first commit

---
+++ app/code/Plazathemes/Loadjs/view/frontend/requirejs-config.js
@@ -0,0 +1,18 @@
+/**
+ * Copyright © 2015 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+  // dependency order if applicable
+  "shim": {
+   "owlcarousel/minjs": ["jquery"],
+   "countdown": ["jquery"],
+   "knob": ["jquery"]
+  },
+  'paths': {
+   'owlcarousel/minjs': 'Plazathemes_Loadjs/js/owl-carousel/owl.carousel.min',
+   'countdown': 'Plazathemes_Loadjs/js/countdown',
+   'knob': 'Plazathemes_Loadjs/js/jquery.knob'
+  }
+ };
